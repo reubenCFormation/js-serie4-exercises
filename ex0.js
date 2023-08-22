fc604c304929856bd21939796699d333fe1052f4
yunities-front <reubenCFormation@gmail.com>
2023-08-22 15:47:18 +0200

    add exo 0

---
+++ ex0.js
@@ -0,0 +1,1 @@
+// dans cette exercise, nous allons vouloir transformer chaque div avec la classe "child". A l'aide du boucle, transformer la couleur du texte entre chaque div avec la classe "child" en bleu. Ensuite, trouver un moyen de transformer la couleur du contenu pour chaqye div de la classe "sub-child" en couleur violet. 
